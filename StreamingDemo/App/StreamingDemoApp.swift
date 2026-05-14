8c11a9a5a8ce92f1831d03af00de8ccd4945f7c6
leo <leo@paradromix.com>
2026-05-14 10:10:55 +0800

    Initial project skeleton with secret config

---
+++ StreamingDemo/App/StreamingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingDemoApp.swift
+//  StreamingDemo
+//
+//  Created by Sean on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct StreamingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
